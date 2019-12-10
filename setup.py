6587b3ea28a80735a11c9c6e3c8ff0e57239b9f1
Reiner Mayers <reiner.mayers@wirag.org>
2019-12-10 22:52:32 +0100

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tkinterInputPanel',
+      version='1.0',
+      description='tkinter input panel created by data',
+      url='https://github.com/mayersre/tkinterInputPanel',
+      author='Reiner Mayers',
+      author_email='urmel20@gmx.de',
+      license='MIT',
+      packages=['tkinterInputPanel'],
+      zip_safe=False)
+
